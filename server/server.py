9ea9ea717ebd25d0b47e48182efc2decb78946cd
Yash Mohod <mohodyash@gmail.com>
2022-08-31 10:05:39 -0400

    initial commit

---
+++ server/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 
+
+@app.route("/login/<email>?<password>")
+def login():
+    return 
+
+@app.route("/dashboard")
+def dashboard():
+    return 
+
+
+@app.route("/logout")
+def logout():
+    return
+
+@app.route("/renewToken")
+def renewToken():
+    return
+
+if __name__ == "__main__":
+    app.run(debug=True)
