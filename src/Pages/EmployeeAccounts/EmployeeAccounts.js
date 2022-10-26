7af34ca8b3f78906188ff6594d7a502348c02fa1
Yash Mohod <mohodyash@gmail.com>
2022-10-26 10:22:46 -0400

    update

--- src/Pages/EmployeeAccounts/EmployeeAccounts.js
+++ src/Pages/EmployeeAccounts/EmployeeAccounts.js
@@ -42,7 +42,7 @@ export default function EmployeeAccounts({setLoggedIn, loggedInUser, autoLogin})
   })
   function inputChangeHandleracc(e) {
     setFormData({...formData,  [e.target.name] : e.target.value})
-    console.log(e.target.name)
+    // console.log(e.target.name)
 }
 async function registerHandler() {
 
@@ -56,7 +56,7 @@ async function registerHandler() {
     collegeId: formData.collegeId,
     dob: formData.dob,
     })
-  console.log(response)
+  // console.log(response)
   if(response.status === 200){
     toast.success(response.message)
     getAccounts()
@@ -182,7 +182,7 @@ async function registerHandler() {
         }
       })
 
-      console.log(curAccount)
+      // console.log(curAccount)
       const oldData = {
         userID:accountId,
         firstName: curAccount.firstName,
@@ -245,10 +245,21 @@ async function registerHandler() {
         return response
     }
 
+    const [org,setOrg] = useState("")
+    const [pos,setPos]= useState("")
+
+    async function getOrgNPos(){
+      const orgres = (await get(API_URL + "/getOrganization?token=" +  localStorage.getItem("token")))
+      const posres = (await get(API_URL + "/getPosition?token=" +  localStorage.getItem("token")))
+      setOrg(orgres["organization"])
+      setPos(posres["position"])
+    }
+
     useEffect(() => {
-        autoLogin()
-        getAccounts()
-        checkMessage()
+      getOrgNPos()
+      autoLogin()
+      getAccounts()
+      checkMessage()
     }, [])
 
     return (
