27a47ebc63ddc4705ab25717bc1d09efcf3c9b4c
Yash Mohod <mohodyash@gmail.com>
2022-11-04 11:10:40 -0400

    add timecard update

--- src/Pages/GeneralPages/TimeCards/TimeCards.js
+++ src/Pages/GeneralPages/TimeCards/TimeCards.js
@@ -12,18 +12,49 @@ import CommonButton from '../../../Components/Buttons/CommonButton'
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
 import TimePicker24H from "../../../Components/TimePicker24H/TimePicker24H"
+import Modal from 'react-bootstrap/Modal';
+
 
 export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
    
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [timeCardData,setTimeCardData] = useState({
+      startDate:"",
+      startTime:"00:00",
+      endDate:"",
+      endTime:"00:00",
+      notes:"",
+    });
+
+    function inputChangeHandlerTimeCardData(e){
+        setTimeCardData({...timeCardData,  [e.target.name] : e.target.value})
+    }
+
+    async function timeCardSubmit(){
+        console.log(timeCardData)
+        let response = await post(API_URL + "/addTimeCard", {
+            token: localStorage.getItem("token"),
+            start:timeCardData.startDate+" "+timeCardData.startTime+":00",
+            end:timeCardData.endDate+" "+timeCardData.endTime+":00",
+            note:timeCardData.notes,
+            })
+
+            if(response.status ==200){
+                toast.success(response.message)
+                handleClose()
+            }else{
+                toast.warning(response.message)
+            }
+    }
 
     useEffect(() => {
         autoLogin()
 
         
     }, [])
-    const [value, onChange] = useState(new Date());
-    // window.addEventListener('resize', handleResize)
-    function inputChangeHandler(){}
 
     return (
         <div className="incident-page">
@@ -31,18 +62,48 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
              <ToastContainer />
             <h1>Time Cards</h1>
             <Form className="incident-form">
-                {/* <Button variant="outline-dark" onClick={() => navigate("/register-accounts")}>Add Account</Button> */}
+                <Button variant="outline-dark" onClick={() => handleShow()}>Add TimeCard</Button>
                 {/* <Button variant="primary" onClick={handleShow}>
                   Launch static backdrop modal
                 </Button> */}
+                
+            </Form>
+
+            <Modal
+              show={show}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Add TimeCard</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+
                 <div className="row ">
                 <div className="col ">
                 <Form.Label className=" d-flex justify-content-start">Start Time </Form.Label>
-                <Form.Control type="date"placeholder="" name="date" />
+                <Form.Control type="date"placeholder="" onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="startDate" />
                 </div>
-                <TimePicker24H inputChangeHandler={ inputChangeHandler} name = {"enrouteTime"}/>
+                <TimePicker24H inputChangeHandler={ inputChangeHandlerTimeCardData} name = {"startTime"}/>
                 </div>
-            </Form>
+                <div className="row ">
+                <div className="col ">
+                <Form.Label className=" d-flex justify-content-start">End Time </Form.Label>
+                <Form.Control type="date"placeholder="" onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="endDate" />
+                </div>
+                <TimePicker24H inputChangeHandler={ inputChangeHandlerTimeCardData} name = {"endTime"}/>
+                </div>
+                <div className="row" id="margin">
+                <Form.Label className=" d-flex justify-content-start">Notes </Form.Label>
+                <Form.Control as="textarea" rows={3} onChange={(e)=>inputChangeHandlerTimeCardData(e)} name = {"notes"}/>
+                </div>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={()=>timeCardSubmit()}>Submit</Button>
+            </Modal.Footer>
+            </Modal>
     
 
         </div>
