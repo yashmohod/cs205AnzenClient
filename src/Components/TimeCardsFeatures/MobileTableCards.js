64ad543636a02cba19f47f759cbbf74829ee158b
Yash Mohod <84158258+yashmohod@users.noreply.github.com>
2022-11-22 01:01:47 -0500

    time card update

--- src/Components/TimeCardsFeatures/MobileTableCards.js
+++ src/Components/TimeCardsFeatures/MobileTableCards.js
@@ -8,33 +8,51 @@ import Accordion from 'react-bootstrap/Accordion';
 export default function MobileTableCards(props) {
     
     const cardRef = useRef(null);
+    const [adminOptions,setAdminOptions]  = useState(false)
+    function approve(){
+        props.approveTimeCard(props.data.id)
+    }
     useEffect(() => {
     
         // console.log(cardRef)
+        setAdminOptions(props.admin)
         if(props.data.approval==="Pending"){
             cardRef.current.firstChild.style.backgroundColor="#fcfcb6"
         }else{
             cardRef.current.firstChild.style.backgroundColor="#a6f7bb"
         }
           
-      }, [])
+      }, [props.data.approval])
    return(<>
    
    <Accordion.Item eventKey={props.keyNum} >
-        <Accordion.Header ref={cardRef}>Submited on: {props.data.submitedDate}</Accordion.Header>
+        <Accordion.Header ref={cardRef} >Submited on: {props.data.submitedDate}</Accordion.Header>
         <Accordion.Body>
         <Form.Label className=" d-flex justify-content-start">Start: {props.data.start} </Form.Label>
         <Form.Label className=" d-flex justify-content-start">End: {props.data.end} </Form.Label>
         <Form.Label className=" d-flex justify-content-start">Duration: {props.data.duration} </Form.Label>
         <Form.Label className=" d-flex justify-content-start">Status: {props.data.approval} </Form.Label>
         <Form.Label className=" d-flex justify-content-start">Note: {props.data.note} </Form.Label>
-        {props.data.approval==="Pending"?<div className="row">
+        {props.data.approval==="Pending" && !adminOptions?<div className="row">
             <div className="col-6">
                 <Button variant={"outline-success"} onClick={()=>{props.editTimeCard(props.data.id)}} className="p-1">Edit</Button>
             </div>
             <div className="col-6">
                 <Button variant={"outline-danger"} onClick={()=>{props.deleteTimeCard(props.data.id)}}className="p-1">Delete</Button>
             </div>
+        </div>: null}
+        {adminOptions?<div className="row">
+            <div className="col-4">
+                <Button variant={"outline-success"} onClick={()=>{props.editTimeCard(props.data.id)}} className="p-1">Edit</Button>
+            </div>
+            <div className="col-4">
+                <Button variant={"outline-danger"} onClick={()=>{props.deleteTimeCard(props.data.id)}}className="p-1">Delete</Button>
+            </div>
+
+            {props.data.approval==="Pending"?
+            <div className="col-4">
+                <Button variant={"outline-primary"} onClick={()=>approve()}className="p-1">Approve</Button>
+            </div>: null}
         </div>: null}
         </Accordion.Body>
       </Accordion.Item>
--- src/Pages/GeneralPages/TimeCards/TimeCards.js
+++ src/Pages/GeneralPages/TimeCards/TimeCards.js
@@ -13,10 +13,11 @@ import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
 import TimePicker24H from "../../../Components/TimePicker24H/TimePicker24H"
 import Modal from 'react-bootstrap/Modal';
-import EmployeeTimeCards from '../../../Components/TimeCardsFeatures/EmployeeTimeCards';
-import AdminTimeCards from '../../../Components/TimeCardsFeatures/AdminTimeCards';
+import MobileTableCards from '../../../Components/TimeCardsFeatures/MobileTableCards';
+
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Accordion from 'react-bootstrap/Accordion';
 
 export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
    
@@ -32,6 +33,7 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
         endDate:"",
         endTime:"00:00",
         note:"",
+        id:"",
       });
 
     function inputChangeHandlerTimeCardData(e){
@@ -39,7 +41,10 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
     }
 
     async function timeCardSubmit(){
-        console.log(timeCardData)
+        if(timeCardData.id !=""){
+          editedtimeCardSubmit()
+        }
+        else{
         let response = await post(API_URL + "/addTimeCard", {
             token: localStorage.getItem("token"),
             start:timeCardData.startDate+" "+timeCardData.startTime+":00",
@@ -50,9 +55,20 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
         if(response.status === 200){
             toast.success(response.message)
             handleClose()
+            
         }else{
             toast.warning(response.message)
         }
+        getAlltc(pos)
+        setTimeCardData({
+          startDate:"",
+          startTime:"00:00",
+          endDate:"",
+          endTime:"00:00",
+          note:"",
+          id:"",
+        })
+      }
     }
     const [org,setOrg] = useState("")
     const [pos,setPos]= useState("")
@@ -61,15 +77,264 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
         const posres = (await get(API_URL + "/getPosition?token=" +  localStorage.getItem("token")))
         setOrg(orgres["organization"])
         setPos(posres["position"])
+        getAlltc(posres["position"])
     }
+    const[myTC,setMYtc]=useState([])
+    const[allTC,setALLtc]=useState([])
+
+    async function getAlltc(position){
+    let responseALL = await get(API_URL + "/getTimeCards?token=" +  localStorage.getItem("token"))
+      
+      if(responseALL.status == 200) {
+        var data = responseALL.UserTimeCards.map((tc)=>{
+          if(tc.approval){
+            tc.approval ="Approved"
+          }else{
+            tc.approval ="Pending"
+          }
+          return(
+            tc
+          )
+        })
+        setMYtc(data)
+    }
+  
+    if(position == "admin"){
+      if(responseALL.status == 200) {
+        var data = responseALL.AlltimeCards.map( (tc)=>{
+         if(tc.approval){
+            tc.approval ="Approved"
+         }else{
+           tc.approval ="Pending"
+         }
+         return(
+           tc
+         )
+        })
+        setALLtc(data)
+      }
+    }
+}
+
+
+function editTimeCard(timeCardID){
+  var data = {}
+  for(var x =0; x< myTC.length;x++){
+    if(myTC[x].id == timeCardID){
+      data=myTC[x]
+    }
+  }
+
+  setTimeCardData({
+    startDate:data.start.split("/")[0].replaceAll(' ', ''),
+    startTime:data.start.split("/")[1].replaceAll(' ', ''),
+    note:data.note,
+    endDate:data.end.split("/")[0].replaceAll(' ', ''),
+    endTime:data.end.split("/")[1].replaceAll(' ', ''),
+    id:timeCardID,
+  })
+
+  handleShow()
+
+ }
+
+ async function editedtimeCardSubmit(){
+  console.log(timeCardData)
+  let response = await post(API_URL + "/editTimeCard", {
+      token: localStorage.getItem("token"),
+      idTimecardID:timeCardData.id,
+      start:timeCardData.startDate+" "+timeCardData.startTime+":00",
+      end:timeCardData.endDate+" "+timeCardData.endTime+":00",
+      note:timeCardData.note,
+      })
+
+  if(response.status === 200){
+      toast.success(response.message)
+      handleClose()
+  }else{
+      toast.warning(response.message)
+  }
+  getAlltc(pos)
+  setTimeCardData({
+    startDate:"",
+    startTime:"00:00",
+    endDate:"",
+    endTime:"00:00",
+    note:"",
+    id:"",
+  })
+}
+
+ async function deleteTimeCard(timeCardID){
+
+  let response = await post(API_URL + "/clearTimeCard", {token: localStorage.getItem("token"),TimecardID: timeCardID})
+  if(response.status === 200){
+    toast.success(response.message)
+  }else{
+    toast.warning(response.message)
+  }
+  getAlltc(pos)
+ }
+
+
+
+ const myTCgridRef = useRef(null);
+ const myTCdefaultColDef= { resizable: true}
+ const myTCcolumnDefs = [
+    {field: 'submitedDate', headerName: 'Submited Date' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'start', headerName: 'Start' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'end', headerName: 'End' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'duration', headerName: ' Duration' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'approval', headerName: 'Status' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'note', headerName: 'Note' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+    {field: 'id',
+    headerName: '' ,
+    cellStyle: { 'textAlign': 'center' },
+    cellRenderer: (params) => {
+      if(params.data.approval == "Pending"){
+      return( <CommonButton buttonText={"Edit"} variant={"outline-success"} clicked={()=>{editTimeCard(params.data.id)}} />)
+      }else{
+        return null
+      }
+    }},
+    {field: 'id',
+    headerName: '' ,
+    cellStyle: { 'textAlign': 'center' },
+    cellRenderer: (params) => {
+      if(params.data.approval == "Pending"){
+        return( <CommonButton buttonText={"Delete"} variant={"outline-danger"} clicked={()=>{deleteTimeCard(params.data.id)}} />)
+        }else{
+          return null
+        }
+    }},
+    ]
+
+
+  function myTCshowMobileViewTimeCards(){
+    let sortedfTC = []
+    for(let x = 0; x< myTC.length; x++){
+      if(myTC[x].approval =="Pending" ){
+        sortedfTC.push(myTC[x])
+      }
+    }
+    for(let x = 0; x< myTC.length; x++){
+      if(myTC[x].approval =="Approved" ){
+        sortedfTC.push(myTC[x])
+      }
+    }
+
+    return(
+      sortedfTC.map(element => {
+      return(
+      <MobileTableCards keyNum ={myTC.indexOf(element)}  data={element} editTimeCard={editTimeCard} deleteTimeCard={deleteTimeCard} admin={false} />
+     ) }))
+  }
+
+
+  // AdminTimeCards
+  async function approveTimeCard(timeCardID){
+    let response = await post(API_URL + "/approveTimeCard", {token: localStorage.getItem("token"),TimecardID: timeCardID})
+    if(response.status === 200){
+      toast.success(response.message)
+    }else{
+      toast.warning(response.message)
+    }
+    console.log(pos)
+    getAlltc(pos)
+
+   }
+
+
+
+   const allTCgridRef = useRef(null);
+   const allTCdefaultColDef= { resizable: true}
+   const allTCcolumnDefs = [
+      {field: 'whoName', headerName: 'Employee' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'submitedDate', headerName: 'Submited Date' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'start', headerName: 'Start' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'end', headerName: 'End' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'duration', headerName: ' Duration' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'approval', headerName: 'Status' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'note', headerName: 'Note' ,cellStyle: { 'textAlign': 'center' }, sortable: true},
+      {field: 'id',
+      headerName: '' ,
+      cellRenderer: CommonButton, 
+      cellStyle: { 'textAlign': 'center' },
+      cellRendererParams: {
+        clicked: function(field) {
+         editTimeCard(field)
+        },
+        buttonText:"Edit",
+        variant:"outline-success",
+      }},
+      {field: 'id',
+      headerName: '' ,
+      cellRenderer: CommonButton, 
+      cellStyle: { 'textAlign': 'center' },
+      cellRendererParams: {
+        clicked: function(field) {
+         deleteTimeCard(field)
+        },
+        buttonText:"Delete",
+        variant:"outline-danger"
+      }},
+      {field: 'id',
+      headerName: '' ,
+      cellRenderer: CommonButton, 
+      cellStyle: { 'textAlign': 'center' },
+      cellRendererParams: {
+        clicked: function(field) {
+         approveTimeCard(field)
+        },
+        buttonText:"Approve",
+        variant:"outline-primary"
+      }},
+      
+      ]
+
+
+    function allTCshowMobileViewTimeCards(){
+      let sortedfTC = []
+      for(let x = 0; x< allTC.length; x++){
+        if(allTC[x].approval =="Pending" ){
+          sortedfTC.push(allTC[x])
+        }
+      }
+      for(let x = 0; x< allTC.length; x++){
+        if(allTC[x].approval =="Approved" ){
+          sortedfTC.push(allTC[x])
+        }
+      }
+
+
+      return(
+        sortedfTC.map(element => {
+        return(
+        <MobileTableCards keyNum ={allTC.indexOf(element)}  data={element} editTimeCard={editTimeCard} deleteTimeCard={deleteTimeCard} approveTimeCard={approveTimeCard}  admin={true}/>
+       ) }))
+    }
+
+
 
     useEffect(() => {
         autoLogin()
         setORGnPOS()
-        
-    }, [])
 
-    const[resize,setresize]= useState(true)
+        
+    }, [ ])
+
+    function showAndClear(){
+      handleShow()
+      setTimeCardData({
+        startDate:"",
+        startTime:"00:00",
+        endDate:"",
+        endTime:"00:00",
+        note:"",
+        id:"",
+      })
+    }
+
 
     return (
         <div className="incident-page" >
@@ -77,7 +342,7 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
              <ToastContainer />
             <h1>Time Cards</h1>
             <Form className="incident-form">
-                <Button variant="outline-dark" onClick={() => handleShow()}>Add Time Card</Button>
+                <Button variant="outline-dark" onClick={() =>showAndClear() }>Add Time Card</Button>
                 
             </Form>
             
@@ -88,44 +353,96 @@ export default function TimeCards({setLoggedIn, loggedInUser, autoLogin}) {
             className="mb-3"
             >
             <Tab eventKey="My Time Cards" title="My Time Cards">
-                <EmployeeTimeCards />
+                {/* desktop view */}
+                <div className="d-none d-xxl-block" >
+                  <div className="ag-theme-alpine incident-grid">
+                    
+                          <AgGridReact
+                            ref={myTCgridRef}
+                            columnDefs={myTCcolumnDefs}
+                            defaultColDef={myTCdefaultColDef}
+                            rowData={myTC}
+                            >
+                          </AgGridReact>
+
+
+                  </div>
+                </div>
+
+                {/* Mobile View */}
+                <div className="d-block d-xxl-none" >
+                  <div className="ag-theme-alpine incident-grid">
+                    
+                  <Accordion defaultActiveKey="0">
+                  {myTCshowMobileViewTimeCards()}
+
+                </Accordion>
+
+                  </div>
+                </div>
             </Tab>
-            {pos == "admin"?
+            {pos == "admin"? 
                 <Tab eventKey="All Time Cards" title="All Time Cards">
-                <AdminTimeCards />
+                   {/* desktop view */}
+                  <div className="d-none d-xxl-block" >
+                    <div className="ag-theme-alpine incident-grid">
+                      
+                            <AgGridReact
+                              ref={allTCgridRef}
+                              columnDefs={allTCcolumnDefs}
+                              defaultColDef={allTCdefaultColDef}
+                              rowData={allTC}
+                              >
+                            </AgGridReact>
+
+
+                    </div>
+                  </div>
+
+                  {/* Mobile View */}
+                  <div className="d-block d-xxl-none" >
+                    <div className="ag-theme-alpine incident-grid">
+                      
+                    <Accordion defaultActiveKey="0" >
+                      {allTCshowMobileViewTimeCards()}
+                  </Accordion>
+
+                    </div>
+                  </div>
                 </Tab>
             :null}
             </Tabs>
             
 
-            <Modal
+            {/* edit pop up */}
+    <Modal
               show={show}
               onHide={handleClose}
               backdrop="static"
               keyboard={false}
             >
               <Modal.Header closeButton>
-                <Modal.Title>Add Time Card</Modal.Title>
+                <Modal.Title>Edit Time Card</Modal.Title>
               </Modal.Header>
               <Modal.Body>
 
                 <div className="row ">
                 <div className="col ">
                 <Form.Label className=" d-flex justify-content-start">Start Time </Form.Label>
-                <Form.Control type="date"placeholder=""  onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="startDate" />
+                <Form.Control type="date"placeholder="" value={timeCardData.startDate} onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="startDate" />
                 </div>
-                <TimePicker24H inputChangeHandler={ inputChangeHandlerTimeCardData} name = {"startTime"}/>
+                <TimePicker24H time={timeCardData.startTime} inputChangeHandler={inputChangeHandlerTimeCardData} name = {"startTime"}/>
                 </div>
                 <div className="row ">
                 <div className="col ">
                 <Form.Label className=" d-flex justify-content-start">End Time </Form.Label>
-                <Form.Control type="date"placeholder="" onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="endDate" />
+                <Form.Control type="date"placeholder="" value={timeCardData.endDate} onChange={(e)=>inputChangeHandlerTimeCardData(e)}  name="endDate" />
                 </div>
-                <TimePicker24H  inputChangeHandler={ inputChangeHandlerTimeCardData} name = {"endTime"}/>
+                <TimePicker24H time={timeCardData.endTime} inputChangeHandler={inputChangeHandlerTimeCardData} name = {"endTime"}/>
                 </div>
                 <div className="row" id="margin">
                 <Form.Label className=" d-flex justify-content-start"><strong>Notes:</strong></Form.Label>
-                <Form.Control   as="textarea" rows={3} onChange={(e)=>inputChangeHandlerTimeCardData(e)} name = {"notes"}/>
+                <Form.Control   as="textarea" value={timeCardData.note} rows={3} onChange={(e)=>inputChangeHandlerTimeCardData(e)} name = {"note"}/>
                 </div>
 
             </Modal.Body>
